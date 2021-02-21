661a1cfa03adfd785534c842814e156df684c5b4
Ayelet Danieli <ayeletd@gmail.com>
2021-02-21 02:03:58 +0200

    fixed ex7

--- weekend project/ex7.js
+++ weekend project/ex7.js
@@ -4,46 +4,38 @@
 // - ForEach
 // - Map
 // Using only for(), array and objects (without other js methods)
-console.log("*** ex 7.1 ***");
-//TODO
+console.log("*** ex 7.1 - Extending Array (map, forEach, filter) ***");
 
-let myFilter = (arr, filterFunc) => {
+Array.prototype.myFilter = function (filterCallback) {
   let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (filterFunc(arr[i])) newArr.push(arr[i]);
+  for (let i=0; i < this.length; i++)
+    if (filterCallback(this[i]))
+      newArr.push(this[i]);
+  return newArr;
+}
+
+Array.prototype.myForEach = function(callback) {
+  for (let i =0; i < this.length; i++){
+    callback(this[i]);
   }
-  return newArr;
-};
+}
 
-// arr.forEach(callback(currentValue[, index[, array]]) {
-//     // execute something
-//   }[, thisArg]);
-// Array.prototype.myForEach = (func) => {
-//     let thisp = arguments[1];  // this is implicit
-//     console.log('length: ', this.length);
-//     for(let i=0; i < thisp.length; i++) {
-//         // func.call(thisp[i]);
-//         func.call(thisp, this[i], i, this);
-//     }
-//  };
-let forEach = (arr, func) => {
-    for (let i = 0; i < arr.length; i++) func(arr[i]);
-};
-
-console.log('implementing myFilter', myFilter([1, 2, 3, 4, 5, 6], (a) => a > 3)); //[4,5,6]
-let arr7 = [0, 1, 2, 3, 4];
-console.log('forEach:');
-forEach(arr7, (a) => console.log("hey ", a));
-
-let myMap = (arr, mappingFunc) => {
+Array.prototype.myMap = function(mapCallback) {
   let newArr = [];
-  for (let i = 0; i < arr.length; i++) newArr.push(mappingFunc(arr[i]));
+  for (let i=0; i < this.length; i++)
+    newArr.push(mapCallback(this[i]));
   return newArr;
-};
+}
+
+
+let testArr =  new Array(1,2,3,4,5,6);
+console.log("myFilter: ", testArr.myFilter(elem => elem > 3)); // 4,5,6
+
+let printDouble = (elem) => (console.log(elem*2));
+console.log("extending forEach: " );
+testArr.myForEach(printDouble); // 2,4,6,8,10
 
 console.log(
   "implementing map:",
-  myMap([1, 2, 3, 4], (a) => a * 2)
-); // [2,4,6,8]
-
-//TODO- implement with object
+  testArr.myMap((a) => a * a));
+ // [1,4,9,16,25,36]
